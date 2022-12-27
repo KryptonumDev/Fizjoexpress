76f13a522f2633755a48ade1174f31a8cc2aecd3
KryptonumDev <kryptonumstudio@gmail.com>
2022-12-27 21:23:44 +0100

    prettier save

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,125 +1,134 @@
-import { graphql } from "gatsby"
-import * as React from "react"
-import Hero from "../components/hero-homepage"
-import Process from "../components/process-homepage"
-import ServicesGrid from "../components/services-grid"
-import TwoColumnFlex from "../components/two-column-flex"
+import { graphql } from 'gatsby'
+import * as React from 'react'
+import Hero from '../components/hero-homepage'
+import Process from '../components/process-homepage'
+import ServicesGrid from '../components/services-grid'
+import TwoColumnFlex from '../components/two-column-flex'
 
-const IndexPage = ({ data: { wpPage: { homepage } } }) => {
+const IndexPage = ({
+  data: {
+    wpPage: { homepage }
+  }
+}) => {
   return (
     <main>
       <Hero data={homepage.sekcjaPowitalnaHomepage} />
-      <TwoColumnFlex data={{
-        header: homepage.sekcjaOFizjoexpress.aboutSmallHeader,
-        title: homepage.sekcjaOFizjoexpress.aboutTitle,
-        text: homepage.sekcjaOFizjoexpress.aboutText,
-        image: homepage.sekcjaOFizjoexpress.aboutVerticalImage,
-        link: homepage.sekcjaOFizjoexpress.ctaLink
-      }} />
-      <ServicesGrid data={homepage.sekcjaCoLeczymyWFizjoexpress} />
-
+      <TwoColumnFlex
+        data={{
+          header:
+            homepage.sekcjaOFizjoexpress.aboutSmallHeader,
+          title: homepage.sekcjaOFizjoexpress.aboutTitle,
+          text: homepage.sekcjaOFizjoexpress.aboutText,
+          image:
+            homepage.sekcjaOFizjoexpress.aboutVerticalImage,
+          link: homepage.sekcjaOFizjoexpress.ctaLink
+        }}
+      />
+      <ServicesGrid
+        data={homepage.sekcjaCoLeczymyWFizjoexpress}
+      />
       <Process data={homepage.sekcjaProcesWspolpracy} />
-    </main >
+    </main>
   )
 }
 
 export default IndexPage
 
 export const query = graphql`
-query homepage {
-  wpPage(id: {eq: "cG9zdDoxNw=="}){
-    id
-    homepage{
-      sekcjaProcesWspolpracy {
-        tytulSekcjiTwoCol
-        tytulSekcji
-        trescPodTytulemTwoCol
-        tresc
-        malyNaglowekTwoColSection
-        malyNaglowek
-        linkPrzyciskuPobocznego {
-          url
-          title
-          target
-        }
-        linkPrzyciskuGlownego {
-          url
-          title
-          target
-        }
-        zdjecieWSekcji{
-          altText
-          localFile{
-            childImageSharp{
-              gatsbyImageData
-            }
+  query homepage {
+    wpPage(id: { eq: "cG9zdDoxNw==" }) {
+      id
+      homepage {
+        sekcjaProcesWspolpracy {
+          tytulSekcjiTwoCol
+          tytulSekcji
+          trescPodTytulemTwoCol
+          tresc
+          malyNaglowekTwoColSection
+          malyNaglowek
+          linkPrzyciskuPobocznego {
+            url
+            title
+            target
           }
-        }
-        etapy {
-          trescPodTytulemOpcjonalna
-          nazwaEtapu
-        }
-      }
-      sekcjaCzekamyWlasnieNaCiebie {
-        tytulSekcji
-        trescPodTytulem
-        malyNaglowekNadTytulem
-        linkPrzycisku {
-          target
-          title
-          url
-        }
-      }
-      sekcjaCoLeczymyWFizjoexpress {
-        tytulSekcji
-        malyNaglowekNadTytulem
-        listaCoLeczymy {
-          podpisPodIkona
-          grafika {
+          linkPrzyciskuGlownego {
+            url
+            title
+            target
+          }
+          zdjecieWSekcji {
             altText
             localFile {
-              publicURL
+              childImageSharp {
+                gatsbyImageData
+              }
+            }
+          }
+          etapy {
+            trescPodTytulemOpcjonalna
+            nazwaEtapu
+          }
+        }
+        sekcjaCzekamyWlasnieNaCiebie {
+          tytulSekcji
+          trescPodTytulem
+          malyNaglowekNadTytulem
+          linkPrzycisku {
+            target
+            title
+            url
+          }
+        }
+        sekcjaCoLeczymyWFizjoexpress {
+          tytulSekcji
+          malyNaglowekNadTytulem
+          listaCoLeczymy {
+            podpisPodIkona
+            grafika {
+              altText
+              localFile {
+                publicURL
+              }
             }
           }
         }
-      }
-      sekcjaPowitalnaHomepage {
-        heroTitle
-        heroText
-        text
-        buttonLink {
-          url
-          title
-          target
-        }
-        zdjecieWTle{
-          altText
-          localFile {
-            childImageSharp {
-              gatsbyImageData
+        sekcjaPowitalnaHomepage {
+          heroTitle
+          heroText
+          text
+          buttonLink {
+            url
+            title
+            target
+          }
+          zdjecieWTle {
+            altText
+            localFile {
+              childImageSharp {
+                gatsbyImageData
+              }
             }
           }
         }
-      }
-      sekcjaOFizjoexpress {
-        aboutSmallHeader
-        aboutTitle
-        aboutText
-        aboutVerticalImage {
-          altText
-          localFile {
-            childImageSharp {
-              gatsbyImageData
+        sekcjaOFizjoexpress {
+          aboutSmallHeader
+          aboutTitle
+          aboutText
+          aboutVerticalImage {
+            altText
+            localFile {
+              childImageSharp {
+                gatsbyImageData
+              }
             }
           }
-        }
-        ctaLink {
-          url
-          title
-          target
+          ctaLink {
+            url
+            title
+            target
+          }
         }
       }
     }
   }
-}
 `
