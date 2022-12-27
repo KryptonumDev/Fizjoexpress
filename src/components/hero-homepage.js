c22be36f4d00364377f6fcfacf9c062c025c1c89
KryptonumDev <kryptonumstudio@gmail.com>
2022-12-27 21:22:50 +0100

    Button primary and underline CSS classes

--- src/components/hero-homepage.js
+++ src/components/hero-homepage.js
@@ -1,64 +1,100 @@
-import { Link } from "gatsby"
-import { GatsbyImage } from "gatsby-plugin-image"
-import React from "react"
-import styled from "styled-components"
-import { Container } from "../atoms/container"
+import { Link } from 'gatsby'
+import { GatsbyImage } from 'gatsby-plugin-image'
+import React from 'react'
+import styled from 'styled-components'
+import { Container } from '../atoms/container'
 import { textParser } from './../helpers/text-parser'
+import { Button } from '../moleculas/link'
 
-export default function Hero({ data: { heroTitle, heroText, text, buttonLink, zdjecieWTle } }) {
-    return (
-        <Wrapper>
-            <Container>
-                <TextBlock>
-                    <span className="text">{text}</span>
-                    <h1 className="main-title" dangerouslySetInnerHTML={{ __html: textParser(heroTitle) }}/>
-                    <p className="text" dangerouslySetInnerHTML={{ __html: textParser(heroText) }}/>
-                    {buttonLink?.url && <Link className="button" to={buttonLink.url} target={buttonLink.target ? buttonLink.target : null}>{buttonLink.title}</Link>}
-                </TextBlock>
-                <GatsbyImage className="image" loading="eager" image={zdjecieWTle.localFile.childImageSharp.gatsbyImageData} alt={zdjecieWTle.altText}/>
-            </Container>
-        </Wrapper>
-    )
+export default function Hero({
+  data: {
+    heroTitle,
+    heroText,
+    text,
+    buttonLink,
+    zdjecieWTle
+  }
+}) {
+  return (
+    <Wrapper>
+      <Container>
+        <TextBlock>
+          <span className='text'>{text}</span>
+          <h1
+            className='main-title'
+            dangerouslySetInnerHTML={{
+              __html: textParser(heroTitle)
+            }}
+          />
+          <p
+            className='text'
+            dangerouslySetInnerHTML={{
+              __html: textParser(heroText)
+            }}
+          />
+          {buttonLink?.url && (
+            <Button
+              className='button button__primary'
+              to={buttonLink.url}
+              target={
+                buttonLink.target ? buttonLink.target : null
+              }>
+              {buttonLink.title}
+            </Button>
+          )}
+        </TextBlock>
+        <GatsbyImage
+          className='image'
+          loading='eager'
+          image={
+            zdjecieWTle.localFile.childImageSharp
+              .gatsbyImageData
+          }
+          alt={zdjecieWTle.altText}
+        />
+      </Container>
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.section`
-    background-color: var(--color-blue);
-    min-height: 768px;
-    position: relative;
-    padding-top: 240px;
-    box-sizing: border-box;
+  background-color: var(--color-blue);
+  min-height: 768px;
+  position: relative;
+  padding-top: 240px;
+  box-sizing: border-box;
 
-    .image{
-        position: absolute;
-        right: 0;
-        top: 0;
-        bottom: 0;
-        z-index: 0;
-        max-height: 100%;
-        height: fit-content;
-        aspect-ratio: 1.1328125/1;
+  .image {
+    position: absolute;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    z-index: 0;
+    max-height: 100%;
+    height: fit-content;
+    aspect-ratio: 1.1328125/1;
 
-        img{
-            max-height: 100%;
-        }
+    img {
+      max-height: 100%;
     }
+  }
 `
 
 const TextBlock = styled.div`
-    position: relative;
-    z-index: 1;
-    max-width: 515px;
-    *{
-        color: var(--color-white);
-    }
-    span{
-        color: var(--color-yellow);
-    }
-    h1{
-        margin-top: 10px;
-        margin-bottom: 20px;
-    }
-    a{
-        margin-top: 30px;
-    }
+  position: relative;
+  z-index: 1;
+  max-width: 515px;
+  * {
+    color: var(--color-white);
+  }
+  span {
+    color: var(--color-blue);
+  }
+  h1 {
+    margin-top: 10px;
+    margin-bottom: 20px;
+  }
+  a {
+    margin-top: 30px;
+  }
 `
--- src/styles/global.js
+++ src/styles/global.js
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from "styled-components"
+import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
 
@@ -10,7 +10,12 @@ const GlobalStyles = createGlobalStyle`
         --color-light-gray: #F6F6F6;
         --color-medium-gray: #E9E9E9;
 
+        --color-transparent: transparent;
+
         --margin-intersection: 100px;
+
+        --button-padding-vertical: 26px;
+        --button-padding-horizontal: 96px;
 
         --box-shadow: 0px 3px 30px #00000016;
     }
@@ -18,6 +23,7 @@ const GlobalStyles = createGlobalStyle`
     *{
         text-decoration: none;
         list-style: none;
+        box-sizing: border-box;
         padding: 0;
         margin: 0;
         color: var(--color-blue);
@@ -57,10 +63,44 @@ const GlobalStyles = createGlobalStyle`
         font-weight: 400;
     }
 
-    .button{
+    .button {
         font-size: 12px;
         line-height: 150%;
         font-weight: 700;
+        display: inline-block;
+        padding: var(--button-padding-vertical) var(--button-padding-horizontal);
+        position: relative;
+        color: var(--color-blue);
+        background-color: var(--color-yellow);
+        border: 0;
+        transition: color .2s ease-out, background-color .2s ease-out;
+        outline: 1px solid var(--color-blue);
+        outline-offset: 2px;
+    }
+
+    .button-underline {
+        background-color: var(--color-transparent);
+        color: var(--color-blue);
+        &:after {
+            content: '';
+            position: absolute;
+            bottom: 13px;
+            left: var(--button-padding-horizontal);
+            height: 2px;
+            width: calc(100% - 2 * var(--button-padding-horizontal));
+            transition: transform .2s ease-out;
+            background-color: var(--color-blue);
+            transform: scaleX(0.5);
+            transform-origin: left center;
+        }
+
+        &:focus-visible:after, &:hover:after {
+            transform: scaleX(1);
+            outline: 1px solid var(--color-blue);
+        }
+        &:focus {
+            outline: none;
+        }
     }
 `
 export default GlobalStyles
