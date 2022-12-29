47ff249c8963a4c6ff85e2a5b2b3d8da5cdf2961
KryptonumDev <kryptonumstudio@gmail.com>
2022-12-29 18:51:05 +0100

    Footer data

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,23 +1,23 @@
-import React from "react"
-import styled from "styled-components"
+import React from 'react'
+import styled from 'styled-components'
+import Footer from '../components/footer'
 import GlobalStyles from './../styles/global'
 
 export default function Layout({ children }) {
-    return (
-        <Wrapper>
-            <GlobalStyles />
-            {/* <Header /> */}
-            {children}
-            {/* <Footer /> */}
-        </Wrapper>
-
-    )
+  return (
+    <Wrapper>
+      <GlobalStyles />
+      {/* <Header /> */}
+      {children}
+      <Footer />
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    height: fit-content;
-    min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: fit-content;
+  min-height: 100vh;
 `
