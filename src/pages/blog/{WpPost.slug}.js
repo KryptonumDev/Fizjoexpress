2e2667e8663a448502d565a3797ccc3514ca03ae
KryptonumDev <kryptonumstudio@gmail.com>
2022-12-29 10:57:42 +0100

    Fields for blog post queried

--- src/pages/blog/{WpPost.slug}.js
+++ src/pages/blog/{WpPost.slug}.js
@@ -1,7 +1,43 @@
+import { graphql } from 'gatsby'
 import React from 'react'
 
-const BlogPost = () => {
-  return <div>BlogPost</div>
+const BlogPost = ({ data: { wpPost } }) => {
+  const {
+    title,
+    terms: { nodes: taxonomies },
+    date
+  } = wpPost
+  const author = taxonomies.find(
+    (el) => el.taxonomyName === 'autor'
+  )
+  const category = taxonomies.find(
+    (el) => el.taxonomyName === 'category'
+  )
+
+  return (
+    <div>
+      {' '}
+      {title}
+      {date}
+      {author.name} | {category.name}
+    </div>
+  )
 }
 
 export default BlogPost
+
+export const blogPostQuery = graphql`
+  query ($id: String) {
+    wpPost(id: { eq: $id }) {
+      title
+      terms {
+        nodes {
+          name
+          taxonomyName
+          slug
+        }
+      }
+      date(formatString: "D.MM.YYYY")
+    }
+  }
+`
