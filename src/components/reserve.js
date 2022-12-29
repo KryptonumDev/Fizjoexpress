82b5c0b8ae0a4b3cb1b5af9ef6c8635a0aa09eb9
KryptonumDev <kryptonumstudio@gmail.com>
2022-12-29 14:23:49 +0100

    BlogPost multiple authors and aside with links to other posts

--- src/components/reserve.js
+++ src/components/reserve.js
@@ -1,171 +1,177 @@
-import React from "react"
-import styled from "styled-components"
-import { Container } from "../atoms/container"
-import { TwoColumnFlexVariants } from "../constants/two-column-flex-variants"
-import { Button } from "../moleculas/link"
-import { TextBlock } from "../organisms/text-block"
+import React from 'react'
+import styled from 'styled-components'
+import { Container } from '../atoms/container'
+import { TwoColumnFlexVariants } from '../constants/two-column-flex-variants'
+import { Button } from '../moleculas/link'
+import { TextBlock } from '../organisms/text-block'
 
-export default function Reserve({ data: { header, title, text, link, steps } }) {
-    return (
-        <Wrapper>
-            <Container>
-                <Content>
-                    <div className="text-wrap">
-                        <TextBlock
-                            header={header}
-                            title={title}
-                            text={text}
-                            link={false}
-                            cytate={false}
-                            variant={TwoColumnFlexVariants.buttonUnderText}
-                        />
-                    </div>
-                    <Steps>
-                        {steps.map((el, index) => (
-                            <Step>
-                                <span className="bix-text">{index + 1}</span>
-                                <p className="text">{el.nazwaKroku}</p>
-                            </Step>
-                        ))}
-                    </Steps>
-                </Content>
-                <div className="button-wrap">
-                    <Button className={'white'}>{link.title}</Button>
-                </div>
-            </Container>
-        </Wrapper>
-    )
+export default function Reserve({
+  data: { header, title, text, link, steps }
+}) {
+  return (
+    <Wrapper>
+      <Container>
+        <Content>
+          <div className='text-wrap'>
+            <TextBlock
+              header={header}
+              title={title}
+              text={text}
+              link={false}
+              cytate={false}
+              variant={
+                TwoColumnFlexVariants.buttonUnderText
+              }
+            />
+          </div>
+          <Steps>
+            {steps.map((el, index) => (
+              <Step key={el.nazwaKroku}>
+                <span className='bix-text'>
+                  {index + 1}
+                </span>
+                <p className='text'>{el.nazwaKroku}</p>
+              </Step>
+            ))}
+          </Steps>
+        </Content>
+        <div className='button-wrap'>
+          <Button className={'white'}>{link.title}</Button>
+        </div>
+      </Container>
+    </Wrapper>
+  )
 }
 
 const Steps = styled.div`
-    display: grid;
-    grid-gap: 20px;
-    position: relative;
+  display: grid;
+  grid-gap: 20px;
+  position: relative;
 
-    margin: var(--margin-intersection) 0 15px 0;
-    margin-right: clamp(0px, ${100 / 1366 * 100}vw ,100px);
+  margin: var(--margin-intersection) 0 15px 0;
+  margin-right: clamp(0px, ${(100 / 1366) * 100}vw, 100px);
 
-    @media (max-width: 1140px) {
-        margin-right: 50px;
-    }
-    @media (max-width: 1024px) {
-        margin-right: 0;
-    }
+  @media (max-width: 1140px) {
+    margin-right: 50px;
+  }
+  @media (max-width: 1024px) {
+    margin-right: 0;
+  }
 
-    @media (max-width: 840px) {
-        margin-top: 0;
-        margin-bottom: 50px;
-    }
+  @media (max-width: 840px) {
+    margin-top: 0;
+    margin-bottom: 50px;
+  }
 
-    &::after{
-        content: '';
-        position: absolute;
-        left: 25px;
-        top: 0;
-        bottom: 0;
-        width: 1px;
-        background-color: var(--color-yellow);
-    }
+  &::after {
+    content: '';
+    position: absolute;
+    left: 25px;
+    top: 0;
+    bottom: 0;
+    width: 1px;
+    background-color: var(--color-yellow);
+  }
 `
 
 const Step = styled.div`
-    display: grid;
+  display: grid;
+  align-items: center;
+  grid-template-columns: 50px auto;
+  grid-gap: 30px;
+
+  span {
+    width: 50px;
+    height: 50px;
+    background-color: var(--color-yellow);
+    color: var(--color-blue);
+    position: relative;
+    z-index: 3;
+
+    display: flex;
+    justify-content: center;
     align-items: center;
-    grid-template-columns: 50px auto;
-    grid-gap: 30px;
+  }
 
-    span{
-        width: 50px;
-        height: 50px;
-        background-color: var(--color-yellow);
-        color: var(--color-blue);
-        position: relative;
-        z-index: 3;
-
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-
-    .text{
-        font-size: 13px;
-        font-weight: 600;
-    }
+  .text {
+    font-size: 13px;
+    font-weight: 600;
+  }
 `
 
 const Wrapper = styled.section`
-    margin-top: var(--margin-intersection);
-    background-color: var(--color-blue);
-    padding-top: 0;
+  margin-top: var(--margin-intersection);
+  background-color: var(--color-blue);
+  padding-top: 0;
 
-    *{
-        color: var(--color-white);
+  * {
+    color: var(--color-white);
+  }
+
+  .white {
+    margin-left: 100px;
+
+    @media (max-width: 1080px) {
+      margin-left: 40px;
     }
-
-    .white{
-        margin-left: 100px;
-
-        @media (max-width: 1080px) {
-            margin-left: 40px;
-        }
-        @media (max-width: 840px){
-            margin-left: 0;
-        }
-    }
-
     @media (max-width: 840px) {
-        .button-wrap{
-            max-width: 500px;
-            margin: 0 auto;
-        }
+      margin-left: 0;
     }
+  }
+
+  @media (max-width: 840px) {
+    .button-wrap {
+      max-width: 500px;
+      margin: 0 auto;
+    }
+  }
 `
 
 const Content = styled.div`
+  display: flex;
+  justify-content: space-between;
+  gap: clamp(30px, ${(100 / 1366) * 100}vw, 100px);
+
+  .text-wrap {
+    position: relative;
+    padding-left: 100px;
+    margin-top: 80px;
     display: flex;
+    flex-direction: column;
     justify-content: space-between;
-    gap: clamp(30px, ${100 / 1366 * 100}vw ,100px);
 
-    .text-wrap{
-        position: relative;
-        padding-left: 100px;
-        margin-top: 80px;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-
-        @media (max-width: 1080px) {
-            padding-left: 40px;
-        }
-
-        .text-block{
-            position: relative;
-            z-index: 2;
-        }
-
-        &::after{
-            content: "";
-            position: absolute;
-            width: 208px;
-            height: 208px;
-            background-color: #212939;
-            top: 0;
-            left: 0;
-            z-index: 0;
-        }
+    @media (max-width: 1080px) {
+      padding-left: 40px;
     }
 
-    @media (max-width: 840px) {
-        flex-direction: column;
-        max-width: 500px;
-        margin: 0 auto;
-
-        .text-wrap{
-            margin-top: var(--margin-intersection);
-            margin-left: -40px;
-            .text{
-                padding-bottom: 0;
-            }
-        }
+    .text-block {
+      position: relative;
+      z-index: 2;
     }
+
+    &::after {
+      content: '';
+      position: absolute;
+      width: 208px;
+      height: 208px;
+      background-color: #212939;
+      top: 0;
+      left: 0;
+      z-index: 0;
+    }
+  }
+
+  @media (max-width: 840px) {
+    flex-direction: column;
+    max-width: 500px;
+    margin: 0 auto;
+
+    .text-wrap {
+      margin-top: var(--margin-intersection);
+      margin-left: -40px;
+      .text {
+        padding-bottom: 0;
+      }
+    }
+  }
 `
--- src/pages/blog/{WpPost.slug}.js
+++ src/pages/blog/{WpPost.slug}.js
@@ -1,13 +1,17 @@
-import { graphql } from 'gatsby'
+import { graphql, Link } from 'gatsby'
 import React from 'react'
+import styled from 'styled-components'
+import { Container } from '../../atoms/container'
 import TwoColumnFlexBlogPost from '../../components/two-column-flex-blog-post'
+import { textParser } from '../../helpers/text-parser'
 
-const BlogPost = ({ data: { wpPost } }) => {
+const BlogPost = ({ data: { wpPost, otherPosts } }) => {
   const {
     title,
     terms: { nodes: taxonomies },
     date,
     excerpt,
+    content,
     seo,
     singlePostData: articleImages
   } = wpPost
@@ -16,12 +20,14 @@ const BlogPost = ({ data: { wpPost } }) => {
     singlePostObrazekWyrozniajacyOgImage: ogImage
   } = articleImages.szablonArtykuluDodatkoweDane
 
-  const authors = taxonomies.filter(
-    (el) => el.taxonomyName === 'autor'
-  )
-  const categories = taxonomies.filter(
-    (el) => el.taxonomyName === 'category'
-  )
+  const authors = taxonomies
+    .filter((el) => el.taxonomyName === 'autor')
+    .slice(0, 2)
+  const categories = taxonomies
+    .filter((el) => el.taxonomyName === 'category')
+    .slice(0, 2)
+
+  const postsToLink = otherPosts.nodes
 
   return (
     <>
@@ -37,16 +43,73 @@ const BlogPost = ({ data: { wpPost } }) => {
           }}
         />
       </header>
-      <main
-        id='blog-content'
-        style={{ marginTop: '300px', height: '200vh' }}>
-        Content here
-      </main>
+      <MainWrapper id='blog-content'>
+        <ContentWrapper
+          dangerouslySetInnerHTML={{
+            __html: content
+          }}
+        />
+        <AsideWrapper>
+          {postsToLink.map((post) => {
+            const {
+              title,
+              slug,
+              excerpt,
+              terms: { nodes: categories }
+            } = post
+
+            const category = categories.filter(
+              (category) => category.name
+            )[0]
+            return (
+              <Link key={post.slug} to={`/blog/${slug}`}>
+                <article>
+                  <span className='post-category'>
+                    {category.name}
+                  </span>
+                  <h3 className='post-header big-text'>
+                    {title}
+                  </h3>
+                  <div
+                    className='post-excerpt'
+                    dangerouslySetInnerHTML={{
+                      __html: textParser(excerpt)
+                    }}
+                  />
+                  <button>Poznaj więcej szczegółów</button>
+                </article>
+              </Link>
+            )
+          })}
+        </AsideWrapper>
+      </MainWrapper>
     </>
   )
 }
 
 export default BlogPost
+
+const CategoryLink = styled(Link)``
+
+const MainWrapper = styled(Container)`
+  max-width: 1366px;
+  margin-top: 100px;
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+  grid-gap: clamp(80px, 8.2vw, 112px);
+  position: relative;
+`
+
+const ContentWrapper = styled.div`
+  margin-left: 100px;
+`
+const AsideWrapper = styled.aside`
+  border: 2px solid;
+  position: sticky;
+  top: 60px;
+  display: flex;
+  flex-direction: column;
+`
 
 export const blogPostQuery = graphql`
   query ($id: String) {
@@ -69,6 +132,7 @@ export const blogPostQuery = graphql`
         canonical
         fullHead
       }
+      content
       singlePostData {
         szablonArtykuluDodatkoweDane {
           obrazekWyrozniajacyNaStroneArtykulu {
@@ -98,6 +162,24 @@ export const blogPostQuery = graphql`
         }
       }
       content
+    }
+    otherPosts: allWpPost(
+      filter: { id: { ne: $id } }
+      limit: 2
+    ) {
+      nodes {
+        terms {
+          nodes {
+            ... on WpCategory {
+              name
+              slug
+            }
+          }
+        }
+        title
+        slug
+        excerpt
+      }
     }
   }
 `
