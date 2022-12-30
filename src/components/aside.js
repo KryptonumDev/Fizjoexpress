8c342fff84b837d58d29aec6bbc60dd85dde2bfc
kryptonumstudio <kryptonumstudio@gmail.com>
2022-12-30 14:04:45 +0100

    aside i polityka prywatnosci

---
+++ src/components/aside.js
@@ -0,0 +1,118 @@
+import { Link } from "gatsby"
+import React from "react"
+import styled from "styled-components"
+import { textParser } from "../helpers/text-parser"
+
+export const Aside = ({posts}) => (
+    <Wrapper>
+        <span className='aside-header big-text'>
+            Dowiedz się jeszcze więcej
+        </span>
+        {posts.map((post) => {
+            const {
+                title,
+                slug,
+                excerpt,
+                terms: { nodes: categories }
+            } = post
+            const category = categories.filter((category) => category.name)[0]
+            return (
+                <Link className='other-post' key={post.slug} to={`/blog/${slug}`}>
+                    <article>
+                        <span className='post-category'>{category.name}</span>
+                        <h3 className='post-header big-text'>{title}</h3>
+                        <div
+                            className='post-excerpt'
+                            dangerouslySetInnerHTML={{
+                                __html: textParser(excerpt)
+                            }}
+                        />
+                        <span className='button button--secondary'>
+                            Poznaj więcej szczegółów
+                        </span>
+                    </article>
+                </Link>
+            )
+        })}
+    </Wrapper>
+)
+
+const Wrapper = styled.aside`
+position: sticky;
+top: 40px;
+max-height: min(80vh, 655px);
+display: flex;
+flex-direction: column;
+gap: 10px;
+padding-right: 100px;
+
+@media (max-width: 1200px) {
+  padding-right: 0;
+}
+
+.aside-header {
+  margin-bottom: 10px;
+}
+.other-post {
+  padding: 20px 30px 16px;
+  background-color: var(--color-light-gray);
+  aspect-ratio: 1/1;
+
+  > article {
+    display: flex;
+    align-items: flex-start;
+    flex-direction: column;
+    gap: 10px;
+    height: 100%;
+  }
+
+  &:hover .button--secondary:after, &:focus-visible .button--secondary:after {
+    transform: scaleX(1);
+  }
+  &:focus {
+    outline: none;
+  }
+  &:focus-visible {
+    outline: 2px solid var(--color-blue);
+  }
+}
+
+.post-category {
+  color: var(--color-yellow);
+  font-weight: bold;
+  font-size: 12px;
+  display: inline-block;
+}
+
+.post-excerpt {
+  margin: 10px 0 6px;
+  font-size: 12px;
+  line-height: 1.7;
+}
+
+.button {
+  margin-top: auto;
+  position: relative;
+  font-size: 12px;
+  font-weight: bold;
+  padding: 4px;
+  margin-left: -4px; 
+  display: inline-block;
+}
+
+.button--secondary:after {
+  content: '';
+  position: absolute;
+  bottom: -4px;
+  left: 4px;
+  width: calc(100% - 8px);
+  height: 2px;
+  background-color: var(--color-blue);
+  transition: transform .15s ease-out;
+  transform-origin: left center;
+  transform: scaleX(0.5);
+  &:hover:after {
+    transform: scaleX(1);
+  }
+}
+`
---
+++ src/pages/polityka-prywatnosci.js
@@ -0,0 +1,85 @@
+import { graphql } from 'gatsby'
+import React from 'react'
+import styled from 'styled-components'
+import { Container } from '../atoms/container'
+import { Aside } from '../components/aside'
+import { ContentWrapper } from './blog/{WpPost.slug}'
+
+const PolitykaPage = ({ data: { otherPosts, wpPage: { politykaPrywatnosci: { tresciPolitykiPrywatnosci } } } }) => {
+    return (
+        <main>
+            <MainWrapper>
+                <div>
+                    <span className='small-header'>{tresciPolitykiPrywatnosci.malyNaglowekNadTytulem}</span>
+                    <h1 className='sub-title'>{tresciPolitykiPrywatnosci.tytulSekcji}</h1>
+                    <ContentWrapper>
+                        <div dangerouslySetInnerHTML={{ __html: tresciPolitykiPrywatnosci.trescPolityki }} />
+                    </ContentWrapper>
+                </div>
+                <Aside posts={otherPosts.nodes} />
+            </MainWrapper>
+        </main>
+    )
+}
+
+export default PolitykaPage
+
+
+const MainWrapper = styled(Container)`
+  max-width: 1366px;
+  margin-top: 40px;
+  padding-top: 60px;
+  padding-bottom: 100px;
+  display: grid;
+  grid-template-columns: 2fr 400px;
+  grid-column-gap: clamp(80px, 8.2vw, 112px);
+  position: relative;
+
+  @media (max-width: 1200px) {
+    grid-template-columns: 2fr 300px;
+  }
+  @media (max-width: 900px) {
+    grid-column-gap: clamp(40px, 6.2vw, 72px);
+  }
+
+  .small-header, .sub-title{
+    margin-left: 100px;
+
+    @media (max-width: 1200px) {
+    margin-left: 0;
+    }
+  }
+  .sub-title{
+    margin-bottom: 30px;
+    margin-top: 10px;
+  }
+`
+
+export const politykasQuery = graphql`
+  query {
+    wpPage(id: { eq: "cG9zdDo0NA==" }){
+        politykaPrywatnosci {
+          tresciPolitykiPrywatnosci {
+            malyNaglowekNadTytulem
+            tytulSekcji
+            trescPolityki
+          }
+        }
+    }
+    otherPosts: allWpPost(limit: 2) {
+      nodes {
+        terms {
+          nodes {
+            ... on WpCategory {
+              name
+              slug
+            }
+          }
+        }
+        title
+        slug
+        excerpt
+      }
+    }
+  }
+`
